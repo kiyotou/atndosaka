17677bb0d44e9d6f5f5bc7b40348ae0669f8d21b
kiyotou <kiyotou@gmail.com>
2011-12-11 00:50:07 +0900

    APIコール＆出力

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -9,9 +9,21 @@ $(function(){
 			null,
 			function(data, status){
 			
+				var html = '';
+			
 				$.each(data.events, function(i, item){
 					
 					alert(item.title);
+					
+					html += '<article>';
+					html += '	<p class="evDate">'+item.started_at+'</p>';
+					html += '	<h2 class="evTitle"><a href="'+item.event_url+'">'+item.title+'</a></h2>';
+					html += '	<p class="evCount"><span class="evAccepted">'+item.accepted+'</span> / <span class="evLimit">'+item.limit+'</span> 人</p>';
+					html += '	<p class="evCatch">'+item.catch+'</p>';
+					html += '	<p class="evPlace">'+item.place+' ('+item.address+')</p>';
+					html += '</article>';
+					
+					$("#evs").html(html);
 					
 				});
 			}
