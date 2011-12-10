341974b3c67ed0aff1869a2f9b257e91d55d5866
kiyotou <kiyotou@gmail.com>
2011-12-11 01:45:27 +0900

    イベント出力整形

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -9,15 +9,20 @@ $(function(){
 			null,
 			function(data, status){
 			
+				var started_at;
 				var html = '';
 			
 				$.each(data.events, function(i, item){
 					
-					alert(item.title);
+					started_at = item.started_at;
+					started_date = started_at.split("T")[0];
+					started_time = started_at.split("T")[1];
+					started_time = started_time.split("+")[0];
+					started_time = started_time.substr(0, started_time.lastIndexOf(":"));
 					
 					html += '<article>';
-					html += '	<p class="evDate">'+item.started_at+'</p>';
-					html += '	<h2 class="evTitle"><a href="'+item.event_url+'">'+item.title+'</a></h2>';
+					html += '	<p class="evDate">'+started_date+' '+started_time+'</p>';
+					html += '	<h2 class="evTitle"><a href="'+item.event_url+'" target="_blank">'+item.title+'</a></h2>';
 					html += '	<p class="evCount"><span class="evAccepted">'+item.accepted+'</span> / <span class="evLimit">'+item.limit+'</span> 人</p>';
 					html += '	<p class="evCatch">'+item.catch+'</p>';
 					html += '	<p class="evPlace">'+item.place+' ('+item.address+')</p>';
